c250f28a6cbf8a6bc853c1d39feb13d5fc6f2cd2
matfcom <matfcom@gmail.com>
2025-04-15 20:30:58 +0200

    Delete components/ui/button.tsx

--- components/ui/button.tsx
+++
@@ -1,8 +0,0 @@
-
-export function Button({ onClick, disabled, children, className }) {
-  return (
-    <button onClick={onClick} disabled={disabled} className={className + " bg-blue-600 text-white rounded-xl"}>
-      {children}
-    </button>
-  );
-}
