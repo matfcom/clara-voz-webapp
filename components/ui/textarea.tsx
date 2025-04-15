eecbac60989ebadb14c7cebebe1cd9a452504164
matfcom <matfcom@gmail.com>
2025-04-15 20:31:08 +0200

    Delete components/ui/textarea.tsx

--- components/ui/textarea.tsx
+++
@@ -1,7 +0,0 @@
-
-export function Textarea({ value, readOnly, placeholder, className }) {
-  return (
-    <textarea value={value} readOnly={readOnly} placeholder={placeholder}
-      className={className + " p-2 border rounded-xl"} rows={4}></textarea>
-  );
-}
