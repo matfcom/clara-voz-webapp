7b6f8decdcfd29c282e37374e11693b2a7d0e019
matfcom <matfcom@gmail.com>
2025-04-20 18:00:53 +0200

    Delete components/ui/button.tsx

--- components/ui/button.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export function Button({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  return (
-    <button className="px-4 py-2 bg-blue-600 text-white rounded" {...props}>
-      {children}
-    </button>
-  );
-}
